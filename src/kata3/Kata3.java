b76af429db7a2bdf4e1fe2853d9b34b372160ec8
Eva <Eva@DESKTOP-R21H1U5>
2020-11-04 13:59:13 +0000

    Histogram Display

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,27 @@
+package kata3;
+
+public class Kata3 {
+
+    public static void main(String[] args) {
+        Histogram<String> histogram = new Histogram();
+        
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        
+        HistogramDisplay histogramDisplay = new HistogramDisplay("Histogram...", histogram);
+        histogramDisplay.execute();
+        
+    
+    }
+    
+}
